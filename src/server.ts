af4035438b2c5e302cf4b40e68a1c89cade80eca
ateeb <mateebsaleemi@gmail.com>
2026-02-20 22:30:08 +0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ dotenv.config();
 
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT || "5000", 10);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
